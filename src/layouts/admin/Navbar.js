f69b8ff12a7a6ce12ff8dc6faccb33209361ab79
bbroger1 <bb.roger@rcssoft.com.br>
2021-12-13 21:34:28 -0300

    exclusão de arquivos demo do start bootstrap

--- src/layouts/admin/Navbar.js
+++ src/layouts/admin/Navbar.js
@@ -6,18 +6,9 @@ const Navbar = () => {
     return (
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <Link className="navbar-brand ps-3" to="#">Start Bootstrap</Link>
-
             <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
                 className="fas fa-bars"></i></button>
-            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div className="input-group">
-                    <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
-                        aria-describedby="btnNavbarSearch" />
-                    <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i
-                        className="fas fa-search"></i></button>
-                </div>
-            </form>
-            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <ul className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0 navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li className="nav-item dropdown">
                     <Link to="#" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false"><i className="fas fa-user fa-fw"></i>
