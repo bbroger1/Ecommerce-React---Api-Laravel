dad959447fc5d84d6c12101fb77b242ebe5de1e1
bbroger1 <bb.roger@rcssoft.com.br>
2021-12-23 22:47:15 -0300

    inclusão de campo de pesquisa e paginação na tabela de produtos

---
+++ src/components/admin/product/ViewProduct copy.js
@@ -0,0 +1,145 @@
+import react, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import swal from "sweetalert";
+import Modal from "./Modal";
+
+function ViewProduct() {
+
+    const [loading, setLoading] = useState(true);
+    const [productList, setProductList] = useState([]);
+    const [productModal, setProductModal] = useState(false);
+    const [tempData, setTempData] = useState([]);
+
+    const columns = ["id", "image", "name", "category", "price", "status"];
+    const [perPage, setPerPage] = useState(10)
+    const [sortColumn, setSortColumn] = useState(columns[0])
+    const [sortOrder, setSortOrder] = useState("asc")
+    const [search, setSearch] = useState("")
+    const [pagination, setPagination] = useState({})
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const handleSort = (column) => {
+        if (column === sortColumn) {
+            sortOrder === sortOrder ? setSortOrder(SORT_DESC) : setSortOrder(SORT_ASC)
+        } else {
+            setSortColumn(column)
+            setSortOrder(sortOrder)
+        }
+    }
+
+    useEffect(() => {
+
+        axios.post(`/api/view-product`)
+            .then(res => {
+                if (res.status === 200) {
+                    setProductList(res.data.products.data);
+                }
+                setLoading(false);
+            })
+            .catch(res => {
+                setLoading(false);
+            })
+    }, []);
+
+    const modalDeleteProduct = (productName, id) => {
+        let tempData = [productName, id];
+        setTempData(item => [...tempData]);
+        return setProductModal(true);
+    }
+
+    const deleteProduct = (e, id) => {
+        e.preventDefault();
+
+        axios.delete(`/api/delete-product/${id}`)
+            .then(res => {
+                if (res.data.status === 200) {
+                    swal("Success", res.data.message, "success");
+                    document.getElementById(id).closest("tr").remove();
+                }
+                setProductModal(false);
+            }).catch(error => {
+                swal("Error", error.message, "error");
+            })
+    }
+
+    var viewProduct_HTMLTABLE = "";
+    if (loading) {
+        return <h3>Loading Product</h3>;
+    } else {
+        viewProduct_HTMLTABLE =
+            productList.map((item) => {
+                return (
+                    <tr key={item.id} id={item.id}>
+                        <td>{item.id}</td>
+                        <td><img src={`http://ecommerce_react.test/${item.image}`} width="50" height="50" alt={item.name}></img></td>
+                        <td>{item.name}</td>
+                        <td>{item.category.name}</td>
+                        <td>{item.selling_price}</td>
+                        <td className="text-center">
+                            {item.status === 1 ? 'Show' : 'Hidden'}
+                        </td>
+                        <td className="text-center">
+                            <Link to={`edit-product/${item.id}`} className="btn btn-primary btn-sm me-3">Edit</Link>
+                            <button
+                                onClick={() => modalDeleteProduct(item.name, item.id)}
+                                className="btn btn-danger btn-sm">
+                                Delete
+                            </button>
+                        </td>
+                    </tr >
+                )
+            })
+    }
+
+    return (
+        <div className="container px-4">
+            <div className="card mt-4">
+                <div className="card-header">
+                    <h3>Product List
+                        <Link to="/admin/add-product" className="btn btn-primary btn-sm float-end">Add Product</Link>
+                    </h3>
+                </div>
+                <div className="card-body">
+                    <table className="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Image</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Price</th>
+                                <th scope="col" className="text-center">Status</th>
+                                <th scope="col" className="text-center">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {viewProduct_HTMLTABLE}
+                        </tbody>
+                    </table>
+                    <div>
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            {productModal === true ?
+                <Modal
+                    productName={tempData[0]}
+                    id={tempData[1]}
+                    deleteProduct={(e) => deleteProduct(e, tempData[1])}
+                    hide={() => setProductModal(false)}
+                /> : ''}
+        </div>
+    )
+
+}
+
+export default ViewProduct;
---
+++ src/components/admin/product/components/DataTable.js
@@ -0,0 +1,177 @@
+import { debounce } from "lodash"
+import React, { useEffect, useRef, useState } from "react"
+import Paginator from "./Paginator"
+import { Link } from "react-router-dom";
+import axios from "axios";
+import swal from "sweetalert";
+import Modal from "../Modal";
+
+const SORT_ASC = "asc"
+const SORT_DESC = "desc"
+
+const DataTable = ({ columns, fetchUrl }) => {
+    const [data, setData] = useState([])
+    const [perPage, setPerPage] = useState(5)
+    const [sortColumn, setSortColumn] = useState(columns[0])
+    const [sortOrder, setSortOrder] = useState("desc")
+    const [search, setSearch] = useState("")
+    const [pagination, setPagination] = useState({})
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const [loading, setLoading] = useState(true)
+
+    const handleSort = (column) => {
+        console.log(sortOrder === SORT_ASC, column === sortColumn);
+        if (column === sortColumn) {
+            sortOrder === SORT_ASC ? setSortOrder(SORT_DESC) : setSortOrder(SORT_ASC)
+        } else {
+            setSortColumn(column)
+            setSortOrder(SORT_ASC)
+        }
+    }
+
+    const handleSearch = useRef(
+        debounce((query) => {
+            setSearch(query)
+            setCurrentPage(1)
+            setSortOrder(SORT_ASC)
+            setSortColumn(columns[0])
+        }, 500)
+    ).current
+
+    const handlePerPage = (perPage) => {
+        setCurrentPage(1)
+        setPerPage(perPage)
+    }
+
+    const loaderStyle = { width: "4rem", height: "4rem" }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            const params = {
+                search,
+                sort_field: sortColumn,
+                sort_order: sortOrder,
+                per_page: perPage,
+                page: currentPage,
+            }
+            const { data } = await axios.get(fetchUrl, { params })
+            setData(data.data)
+            setPagination(data.meta)
+            setTimeout(() => {
+                setLoading(false)
+            }, 300)
+        }
+
+        fetchData()
+    }, [perPage, sortColumn, sortOrder, search, currentPage])
+
+    return (
+        <div>
+            {/* Search per page starts */}
+            <div className="row mb-3">
+                <div className="col-md-3">
+                    <div className="input-group">
+                        <input
+                            className="form-control"
+                            placeholder="Search..."
+                            type="search"
+                            onChange={(e) => handleSearch(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="col-md-2">
+                    <div className="input-group">
+                        <label className="mt-2 me-2">Per page</label>
+                        <select className="form-select" value={perPage} onChange={(e) => handlePerPage(e.target.value)}>
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            {/* Search per page ends  */}
+
+            <div className="tableFixHead">
+                <table className="table table-hover">
+                    <thead className="table-dark">
+                        <tr>
+                            {columns.map((column) => {
+                                return (
+                                    <th key={column} onClick={(e) => handleSort(column)}>
+                                        {column.toUpperCase().replace("_", " ")}
+                                        {column === sortColumn ? (
+                                            <span>
+                                                {sortOrder === SORT_ASC ? (
+                                                    <i className="ms-1 fa fa-arrow-up" aria-hidden="true"></i>
+                                                ) : (
+                                                    <i className="ms-1 fa fa-arrow-down" aria-hidden="true"></i>
+                                                )}
+                                            </span>
+                                        ) : null}
+                                    </th>
+                                )
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.length === 0 ? (
+                            <tr>
+                                <td colSpan={columns.length}>No items found</td>
+                            </tr>
+                        ) : ("")}
+
+                        {!loading ? (
+                            data.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.id}</td>
+                                        <td><img src={`http://ecommerce_react.test/${item.image}`} width="50" height="50" alt={item.name}></img></td>
+                                        <td>{item.name}</td>
+                                        <td>{item.category}</td>
+                                        <td>{item.selling_price}</td>
+                                        <td className="text-center">
+                                            {item.status === 1 ? 'Show' : 'Hidden'}
+                                        </td>
+                                        <td className="text-center">
+                                            <Link to={`edit-product/${item.id}`} className="btn btn-primary btn-sm me-3">Edit</Link>
+                                            <button
+                                                //onClick={() => modalDeleteProduct(item.name, item.id)}
+                                                className="btn btn-danger btn-sm">
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        ) : (
+                            <tr>
+                                <td colSpan={columns.length + 1}>
+                                    <div className="d-flex justify-content-center">
+                                        <div className="spinner-border" style={loaderStyle} role="status">
+                                            <span className="sr-only">Loading...</span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+            {data.length > 0 && !loading ? (
+                <div className="mt-2">
+                    <Paginator
+                        pagination={pagination}
+                        pageChanged={(page) => setCurrentPage(page)}
+                        totalItems={data.length}
+                    />
+                </div>
+            ) : null}
+        </div>
+    )
+}
+
+export default DataTable
